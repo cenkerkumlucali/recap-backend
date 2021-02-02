a673478a68d4e147e664a21cfd11032fd94083aa
Cenker Kumlucalı <cenkerkumlucali0hotmail.com>
2021-02-02 17:12:04 +0300

    2.02.2021

---
+++ Entities/Concrete/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class Color:IEntity
+    {
+        public int CalorId { get; set; }
+        public string CalorName { get; set; }
+    }
+}
